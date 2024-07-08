4427eb266ef14daa5621ad23e8a7bd5e566ef077
swetha040701 <swetha.r@deeveesha.com>
2024-07-08 12:37:58 +0530

    product page

--- php/getbyproductname.php
+++ php/getbyproductname.php
@@ -1,7 +1,7 @@
 <?php
     include "config.php";
     $productName = $_POST["name"];
-		 $sql = "SELECT productName,productId,unit,volume,imgPath_3,imgPath_4,imgPath_5,imgPath_6,productDescription,ingredients,shelfLife,packing,sourcedFrom,Highlight1,Highlight2,Highlight3,Highlight4 FROM products WHERE productName='$productName' AND isActive='1'";
+		 $sql = "SELECT productName,category,productId,unit,volume,imgPath_3,imgPath_4,imgPath_5,imgPath_6,productDescription,ingredients,shelfLife,packing,sourcedFrom,Highlight1,Highlight2,Highlight3,Highlight4 FROM products WHERE productName='$productName' AND isActive='1'";
     $res = $con->query($sql);
 
     if($res->num_rows>0){
