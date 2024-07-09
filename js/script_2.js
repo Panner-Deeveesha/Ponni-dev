8967025bc845ade791d31f4690d5b5721142a1f2
Panner-Deeveesha <panner.selvam@deeveesha.com>
2024-07-09 18:34:19 +0530

    image alignment in product page

--- js/script_2.js
+++ js/script_2.js
@@ -1065,17 +1065,17 @@ var unitAbbreviation = unitMapping[input[0].unit] || input[0].unit;
   document.getElementById("cate-des").innerHTML=dyul;
 
 
-  s2 +='<div class="carousel-item active" id="firstscrollimg" style="width:80%;height:80%;">';
-  s2 +='<img src="' +input[0].imgPath_3+'" width="100%" height="100%">'
+  s2 +='<div class="carousel-item active" id="firstscrollimg" >';
+  s2 +='<img src="' +input[0].imgPath_3+'" >'
   s2 +='</div>';
-  s2 +='<div class="carousel-item" id="secondscrollimg" style="width:80%;height:80%;">';
-  s2 +='<img src="' +input[0].imgPath_4+'" width="100%" height="100%">'
+  s2 +='<div class="carousel-item" id="secondscrollimg" >';
+  s2 +='<img src="' +input[0].imgPath_4+'" >'
   s2 +='</div>';
-  s2 +='<div class="carousel-item" id="thirdscrollimg" style="width:80%;height:80%;">';
-  s2 +='<img src="' +input[0].imgPath_5+'" width="100%" height="100%">'
+  s2 +='<div class="carousel-item" id="thirdscrollimg" >';
+  s2 +='<img src="' +input[0].imgPath_5+'" >'
   s2 +='</div>';
-  s2 +='<div class="carousel-item" id="fourthscrollimg" style="width:80%;height:80%;">';
-  s2 +='<img src="' +input[0].imgPath_6+'" width="100%" height="100%">'
+  s2 +='<div class="carousel-item" id="fourthscrollimg" >';
+  s2 +='<img src="' +input[0].imgPath_6+'" >'
   s2 +='</div>';
 
   document.getElementById("slidecarouselimg").innerHTML=s2;
