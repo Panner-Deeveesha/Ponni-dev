946a6e3730cf0f1b7032f666efc78e2c8794c655
Keerthana312001 <keerthana.n@deeveesha.com>
2024-07-18 10:47:35 +0530

    added mailid for subscribe

--- php/subscribeaction.php
+++ php/subscribeaction.php
@@ -26,14 +26,14 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'amarajk27@gmail.com';                     //SMTP username
-    $mail->Password   = 'ofgnfthytusmrnys';                               //SMTP password
+    $mail->Username   = 'kps.kps.ps@gmail.com';                     //SMTP username
+    $mail->Password   = 'mtal uhln ixmy ntqb';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 	
     //Recipients
-    $mail->setFrom('amarajk27@gmail.com', 'Contact form');
-    $mail->addAddress('amaradharanisss@gmail.com', 'dharani amara');     //Add a recipient
+    $mail->setFrom('kps.kps.ps@gmail.com', 'New Subscriber');
+    $mail->addAddress('keerthananeelamegam2001@gmail.com', 'Keerthana');     //Add a recipient
     
 
    // $e_body = "You have been contacted by $name with regards, their additional message is as follows." . PHP_EOL . PHP_EOL;
@@ -44,16 +44,16 @@ try {
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = "$emailInput has contacted you via Your Website.".PHP_EOL;
+    $mail->Subject = "New Subscriber for Your Website.".PHP_EOL;
     //$mail->Body    = $name. PHP_EOL. $phone. PHP_EOL.$description. PHP_EOL;
-    $mail->Body    = "<br>Email : $emailInput".PHP_EOL;
+    $mail->Body    = "<br>Hello, I am $emailInput, You are running successful website and I wish to subscribe it!".PHP_EOL;
     
 
     $mail->send();
     $message = "Email Sent Successfully, your message has been submitted to us.";
-    echo "<script type='text/javascript'>alert('$message');window.location.href='contact.html';</script>";
+    echo "<script type='text/javascript'>alert('$message');window.location.href='index.html';</script>";
 } catch (Exception $e) {
     $message = "ERROR! Email Sent Unsuccessfull.";
-    echo "<script type='text/javascript'>alert('$message');window.location.href='contact.html';</script>";
+    echo "<script type='text/javascript'>alert('$message');window.location.href='index.html';</script>";
 }
 ?>
